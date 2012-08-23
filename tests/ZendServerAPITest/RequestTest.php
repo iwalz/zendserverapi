db99e9a84bff697d3f868d645c1f953bc263ecfe
ingo <ingo.walz@googlemail.com>
2012-08-23 22:45:04 +0200

    - Fixed tests - Remove container
    Change-Id: I17cb8501c90343b212ae5a3d80f653c7bebc9580

--- tests/ZendServerAPITest/RequestTest.php
+++ tests/ZendServerAPITest/RequestTest.php
@@ -5,11 +5,26 @@ namespace ZendServerAPITest;
  * test case.
  */
 class RequestTest extends \PHPUnit_Framework_TestCase {
-	
+
+    private $apiKey = null;
+    private $config = null;
+    
+    public function setUp()
+    {
+        $this->apiKey = new \ZendServerAPI\ApiKey();
+        $this->apiKey->setName('api');
+        $this->apiKey->setKey('058b82f191d934a7bfe17d12060dd3320869f132d3428fa19d35463903673eee');
+        $this->apiKey->setState(\ZendServerAPI\ApiKey::FULL);
+        
+        $this->config = new \ZendServerAPI\Config;
+        $this->config->setHost('127.0.0.1');
+        $this->config->setApiKey($this->apiKey);
+    }
+    
 	public function testRequest()
 	{
 	    $request = new \ZendServerAPI\Request();
-		$request->setConfig(\ZendServerAPITest\Container::getConfig());
+		$request->setConfig($this->config);
 		$this->assertInstanceOf('\ZendServerAPI\ApiKey', $request->getConfig()->getApiKey());
 		$this->assertSame('api', $request->getConfig()->getApiKey()->getName());
 		$this->assertSame('058b82f191d934a7bfe17d12060dd3320869f132d3428fa19d35463903673eee', $request->getConfig()->getApiKey()->getKey());

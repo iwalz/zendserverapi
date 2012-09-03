3e167aa1c1ed35bba562192206f7fc8b39b0cac1
ingo <ingo.walz@googlemail.com>
2012-09-03 20:26:54 +0200

    Changed some coding styles
    Change-Id: I92d088c6c4e7b44cece9ef72391823b5a063c64b

--- src/ZendServerAPI/Mapper/ServerInfo.php
+++ src/ZendServerAPI/Mapper/ServerInfo.php
@@ -14,37 +14,23 @@ class ServerInfo extends \ZendServerAPI\Mapper\Mapper
         $xml = simplexml_load_string($xml);
         
         $server = new ServerInfoData();
-        $server->setId(
-                (int)$xml->responseData->serverInfo->id
-        );
-        $server->setName(
-                (string)$xml->responseData->serverInfo->name
-        );
-        $server->setAddress(
-                (string)$xml->responseData->serverInfo->address
-        );
-        $server->setStatus(
-                (string)$xml->responseData->serverInfo->status
-        );
+        $server->setId((int)$xml->responseData->serverInfo->id);
+        $server->setName((string)$xml->responseData->serverInfo->name);
+        $server->setAddress((string)$xml->responseData->serverInfo->address);
+        $server->setStatus((string)$xml->responseData->serverInfo->status);
         
         $messageList = new MessageList();
         if(isset($xml->responseData->serverInfo->messageList->error))
         {
-            $messageList->setError(
-                (string)$xml->responseData->serverInfo->messageList->error
-            );
+            $messageList->setError((string)$xml->responseData->serverInfo->messageList->error);
         }
         if(isset($xml->responseData->serverInfo->messageList->info))
         {
-            $messageList->setInfo(
-                (string)$xml->responseData->serverInfo->messageList->info
-            );
+            $messageList->setInfo((string)$xml->responseData->serverInfo->messageList->info);
         }
         if(isset($xml->responseData->serverInfo->messageList->warning))
         {
-            $messageList->setWarning(
-                (string)$xml->responseData->serverInfo->messageList->warning
-            );
+            $messageList->setWarning((string)$xml->responseData->serverInfo->messageList->warning);
         }
         $server->setMessageList($messageList);
 
--- src/ZendServerAPI/Mapper/SystemInfo.php
+++ src/ZendServerAPI/Mapper/SystemInfo.php
@@ -14,56 +14,26 @@ class SystemInfo extends Mapper
     {
         $xml = simplexml_load_string($xml);
         $systemInfo = new SystemInfoData();
-        $systemInfo->setStatus(
-                (string)$xml->responseData->systemInfo->status
-        );
-        $systemInfo->setEdition(
-                (string)$xml->responseData->systemInfo->edition
-        );
-        $systemInfo->setZendServerVersion(
-                (string)$xml->responseData->systemInfo->zendServerVersion
-        );
-        $systemInfo->setSupportedApiVersions(
-                (string)trim($xml->responseData->systemInfo->supportedApiVersions)
-        );
-        $systemInfo->setPhpVersion(
-                (string)$xml->responseData->systemInfo->phpVersion
-        );
-        $systemInfo->setOperatingSystem(
-                (string)$xml->responseData->systemInfo->operatingSystem
-        );
-        $systemInfo->setDeploymentVersion(
-                (string)$xml->responseData->systemInfo->deploymentVersion
-        );
+        $systemInfo->setStatus((string)$xml->responseData->systemInfo->status);
+        $systemInfo->setEdition((string)$xml->responseData->systemInfo->edition);
+        $systemInfo->setZendServerVersion((string)$xml->responseData->systemInfo->zendServerVersion);
+        $systemInfo->setSupportedApiVersions((string)trim($xml->responseData->systemInfo->supportedApiVersions));
+        $systemInfo->setPhpVersion((string)$xml->responseData->systemInfo->phpVersion);
+        $systemInfo->setOperatingSystem((string)$xml->responseData->systemInfo->operatingSystem);
+        $systemInfo->setDeploymentVersion((string)$xml->responseData->systemInfo->deploymentVersion);
         
         $serverLicenseInfo = new LicenseInfo();
-        $serverLicenseInfo->setStatus(
-                (string)$xml->responseData->systemInfo->serverLicenseInfo->status
-        );
-        $serverLicenseInfo->setOrderNumber(
-                (string)$xml->responseData->systemInfo->serverLicenseInfo->orderNumber
-        );
-        $serverLicenseInfo->setValidUntil(
-                (string)$xml->responseData->systemInfo->serverLicenseInfo->validUntil
-        );
-        $serverLicenseInfo->setServerLimit(
-                (string)$xml->responseData->systemInfo->serverLicenseInfo->serverLimit
-        );
+        $serverLicenseInfo->setStatus((string)$xml->responseData->systemInfo->serverLicenseInfo->status);
+        $serverLicenseInfo->setOrderNumber((string)$xml->responseData->systemInfo->serverLicenseInfo->orderNumber);
+        $serverLicenseInfo->setValidUntil((string)$xml->responseData->systemInfo->serverLicenseInfo->validUntil);
+        $serverLicenseInfo->setServerLimit((string)$xml->responseData->systemInfo->serverLicenseInfo->serverLimit);
         $systemInfo->setServerLincenseInfo($serverLicenseInfo);
          
         $managerLicenseInfo = new LicenseInfo();
-        $managerLicenseInfo->setStatus(
-                (string)$xml->responseData->systemInfo->managerLicenseInfo->status
-        );
-        $managerLicenseInfo->setOrderNumber(
-                (string)$xml->responseData->systemInfo->managerLicenseInfo->orderNumber
-        );
-        $managerLicenseInfo->setValidUntil(
-                (string)$xml->responseData->systemInfo->managerLicenseInfo->validUntil
-        );
-        $managerLicenseInfo->setServerLimit(
-                (string)$xml->responseData->systemInfo->managerLicenseInfo->serverLimit
-        );
+        $managerLicenseInfo->setStatus((string)$xml->responseData->systemInfo->managerLicenseInfo->status);
+        $managerLicenseInfo->setOrderNumber((string)$xml->responseData->systemInfo->managerLicenseInfo->orderNumber);
+        $managerLicenseInfo->setValidUntil((string)$xml->responseData->systemInfo->managerLicenseInfo->validUntil);
+        $managerLicenseInfo->setServerLimit((string)$xml->responseData->systemInfo->managerLicenseInfo->serverLimit);
         $systemInfo->setManagerLicenseInfo($managerLicenseInfo);
          
         $messageList = new MessageList();

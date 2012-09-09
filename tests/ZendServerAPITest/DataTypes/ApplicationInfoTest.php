4d60bc0010c3fb475eebb01cadac8c4008cf02bd
ingo <ingo.walz@googlemail.com>
2012-09-09 23:54:56 +0200

    Added some test assets
    Change-Id: I82592aafa9e2ca74a5deba4ca920cb9568134908

---
+++ tests/ZendServerAPITest/DataTypes/ApplicationInfoTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace ZendServerAPITest\DataTypes;
+
+/**
+ * test case.
+ */
+class ApplicationInfoTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp ()
+    {
+
+    }
+    
+    public function testGettersAndSetters()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
+
---
+++ tests/ZendServerAPITest/DataTypes/ApplicationListTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace ZendServerAPITest\DataTypes;
+
+/**
+ * test case.
+ */
+class ApplicationListTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp ()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGettersAndSetters()
+    {
+        $this->markTestIncomplete();
+    }
+}
+

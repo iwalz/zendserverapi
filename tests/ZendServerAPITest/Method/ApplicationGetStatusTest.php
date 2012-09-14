3c7b4ac25c80bba3f6a497e3217ce550b4ce2507
ingo <ingo.walz@googlemail.com>
2012-09-14 23:13:43 +0200

    Fixed static declaration
    Change-Id: I95743edab7afa4bb0b9ef2f7937a51cfe731bec2

--- tests/ZendServerAPITest/Method/ApplicationGetStatusTest.php
+++ tests/ZendServerAPITest/Method/ApplicationGetStatusTest.php
@@ -12,7 +12,7 @@ class ApplicationGetStatusTest extends \PHPUnit_Framework_TestCase
        $this->assertEquals("/ZendServerManager/Api/applicationGetStatus?applications%5B0%5D=1&applications%5B1%5D=2", $method->getLink());
     }
     
-    public function getResponse()
+    public static function getResponse()
     {
         return file_get_contents(__DIR__.'/../DataTypes/TestAsset/applicationlist.xml');
     }

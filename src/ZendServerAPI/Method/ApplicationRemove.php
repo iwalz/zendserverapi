781e76250895165d72361942645287297adb116a
ingo <ingo.walz@googlemail.com>
2012-09-15 23:24:46 +0200

    Implementation of applicationRemove method
    Change-Id: I4b397210dfc4f909d367d6868254b587e827efaa

---
+++ src/ZendServerAPI/Method/ApplicationRemove.php
@@ -0,0 +1,44 @@
+<?php
+namespace ZendServerAPI\Method;
+
+class ApplicationRemove extends \ZendServerAPI\Method
+{
+    /**
+     * ApplicationId to remove
+     * @var int
+     */
+    private $applicationId = null;
+    
+    /**
+     * Constructor for ApplicationRemove method
+     * 
+     * @param int $applicationId ApplicationId to remove
+     */
+    public function __construct($applicationId)
+    {
+        $this->applicationId = $applicationId;
+        parent::__construct();
+    }
+    
+    /**
+     * @see \ZendServerAPI\Method::configure()
+     */
+    public function configure ()
+    {
+        $this->setMethod('POST');
+        $this->setFunctionPath('/ZendServerManager/Api/applicationRemove');
+        $this->setParser(new \ZendServerAPI\Mapper\ApplicationInfo());
+    }
+
+    /**
+     * Content for POST request
+     * 
+     * @return string
+     */
+    public function getContent()
+    {
+        return ("appId=".$this->applicationId);
+    }
+}
+
+?>

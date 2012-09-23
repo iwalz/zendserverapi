0712ce0ae5953d4332300bcc14fe826bb50778ab
ingo <ingo.walz@googlemail.com>
2012-09-23 19:23:59 +0200

    Full Server factory implementation
    Change-Id: I5890a9173bf210c5c42ab4b36f4d5c914b33ecb8

--- src/ZendServerAPI/Factories/ApiVersion10CommandFactory.php
+++ src/ZendServerAPI/Factories/ApiVersion10CommandFactory.php
@@ -10,11 +10,32 @@ class ApiVersion10CommandFactory implements CommandFactory
         
         switch($name)
         {
+            case 'clusterGetServerStatus':
+                return new \ZendServerAPI\Method\ClusterGetServerStatus($args[0]);
+                break;
             case 'clusterAddServer':
                 return new \ZendServerAPI\Method\ClusterAddServer($args[0], $args[1], $args[2]);
                 break;
+            case 'clusterRemoveServer':
+                return new \ZendServerAPI\Method\ClusterRemoveServer($args[0], $args[1]);
+                break;
+            case 'clusterEnableServer':
+                return new \ZendServerAPI\Method\ClusterEnableServer($args[0]);
+                break;
+            case 'clusterDisableServer':
+                return new \ZendServerAPI\Method\ClusterDisableServer($args[0]);
+                break;
+            case 'clusterReconfigureServer':
+                return new \ZendServerAPI\Method\ClusterReconfigureServer($args[0]);
+                break;
+            case 'restartPHP':
+                return new \ZendServerAPI\Method\RestartPHP($args[0], $args[1]);
+                break;
+            case 'getSystemInfo':
+                return new \ZendServerAPI\Method\GetSystemInfo();
+                break;
             default:
-                ;
+                throw new \RuntimeException('The method ' . $name . ' is not available');
         }
     }
 }
--- tests/ZendServerAPITest/Factories/WebApiVersionFactoryTest.php
+++ tests/ZendServerAPITest/Factories/WebApiVersionFactoryTest.php
@@ -18,9 +18,29 @@ class WebApiVersionFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($config->getApiVersion(), \ZendServerAPI\Version::ZS56);
         
         $retFactory = $webApiVersionFactory->getCommandFactory();
-        $method = $retFactory::factory('clusterAddServer', 'example62', 'http://example62:10081/ZendServer', 'test');
+        $this->assertInstanceOf('\ZendServerAPI\Factories\ApiVersion12CommandFactory', $retFactory);
+        $this->assertEquals('ZendServerAPI\Factories\ApiVersion12CommandFactory', get_class($retFactory));
         
-        $this->assertInstanceOf('\ZendServerAPI\Method\ClusterAddServer', $method);
+        $clusterAddServer = $retFactory::factory('clusterAddServer', 'example62', 'http://example62:10081/ZendServer', 'test');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ClusterAddServer', $clusterAddServer);
+        
+        $clusterRemoveServer = $retFactory::factory('clusterRemoveServer', '1', true);
+        $this->assertInstanceOf('\ZendServerAPI\Method\ClusterRemoveServer', $clusterRemoveServer);
+        $this->assertEquals('serverId=1&force=TRUE', $clusterRemoveServer->getContent());
+        
+        $clusterEnableServer = $retFactory::factory('clusterEnableServer', '1');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ClusterEnableServer', $clusterEnableServer);
+        $this->assertEquals('serverId=1', $clusterEnableServer->getContent());
+        
+        $clusterDisableServer = $retFactory::factory('clusterDisableServer', '1');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ClusterDisableServer', $clusterDisableServer);
+        $this->assertEquals('serverId=1', $clusterDisableServer->getContent());
+        
+        $clusterGetServerStatus = $retFactory::factory('clusterGetServerStatus', array(1));
+        $this->assertInstanceOf('\ZendServerAPI\Method\ClusterGetServerStatus', $clusterGetServerStatus);
+        
+        $getSystemInfo = $retFactory::factory('getSystemInfo');
+        $this->assertInstanceOf('\ZendServerAPI\Method\GetSystemInfo', $getSystemInfo);
     }
 }
 

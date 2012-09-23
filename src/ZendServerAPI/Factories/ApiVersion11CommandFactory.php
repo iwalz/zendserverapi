97221e4be4c8a72cda34aea3f534ed4bb9d3b89a
ingo <ingo.walz@googlemail.com>
2012-09-23 21:54:44 +0200

    deployment factory integration
    Change-Id: I1c9c7e949f1d082d16fa5ae04b62e3584ca3d2be

--- src/ZendServerAPI/Factories/ApiVersion11CommandFactory.php
+++ src/ZendServerAPI/Factories/ApiVersion11CommandFactory.php
@@ -1,6 +1,18 @@
 <?php
 namespace ZendServerAPI\Factories;
 
+use ZendServerAPI\Method\ApplicationUpdate;
+
+use ZendServerAPI\Method\ApplicationSynchronize;
+
+use ZendServerAPI\Method\ApplicationRollback;
+
+use ZendServerAPI\Method\ApplicationRemove;
+
+use ZendServerAPI\Method\ApplicationDeploy;
+
+use ZendServerAPI\Method\ApplicationGetStatus;
+
 class ApiVersion11CommandFactory extends ApiVersion10CommandFactory
 {
     public static function factory($name)
@@ -10,6 +22,24 @@ class ApiVersion11CommandFactory extends ApiVersion10CommandFactory
 
         switch($name)
         {
+            case 'applicationGetStatus':
+                return new ApplicationGetStatus($args[0]);
+                break;
+            case 'applicationDeploy':
+                return new ApplicationDeploy($args[0], $args[1]);
+                break;
+            case 'applicationRemove':
+                return new ApplicationRemove($args[0]);
+                break;                
+            case 'applicationRollback':
+                return new ApplicationRollback($args[0]);
+                break;
+            case 'applicationSynchronize':
+                return new ApplicationSynchronize($args[0]);
+                break;
+            case 'applicationUpdate':
+                return new ApplicationUpdate($args[0], $args[1]);
+                break;
             default:
                 return call_user_func_array('parent::factory', array_merge(array($name), $args));
         }
--- tests/ZendServerAPITest/Factories/WebApiVersionFactoryTest.php
+++ tests/ZendServerAPITest/Factories/WebApiVersionFactoryTest.php
@@ -1,6 +1,10 @@
 <?php
 namespace ZendServerAPITest;
 
+use ZendServerAPI\Method\ApplicationUpdate;
+
+use ZendServerAPI\Method\ApplicationDeploy;
+
 use ZendServerAPI\Startup;
 
 /**
@@ -41,6 +45,55 @@ class WebApiVersionFactoryTest extends \PHPUnit_Framework_TestCase
         
         $getSystemInfo = $retFactory::factory('getSystemInfo');
         $this->assertInstanceOf('\ZendServerAPI\Method\GetSystemInfo', $getSystemInfo);
+    }
+    
+    public function testWebApi11VersionFactory()
+    {
+        $request = Startup::getRequest("ZS55");
+        $config = $request->getConfig();
+        $webApiVersionFactory = new \ZendServerAPI\Factories\WebApiVersionFactory();
+        $webApiVersionFactory->setConfig($config);
+    
+        $this->assertEquals($config->getApiVersion(), \ZendServerAPI\Version::ZS55);
+    
+        $retFactory = $webApiVersionFactory->getCommandFactory();
+        $this->assertInstanceOf('\ZendServerAPI\Factories\ApiVersion11CommandFactory', $retFactory);
+        $this->assertEquals('ZendServerAPI\Factories\ApiVersion11CommandFactory', get_class($retFactory));
+    
+        $applicationDeploy = $retFactory::factory('applicationDeploy', 'mypackage.zpk', 'http://www.test.com');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ApplicationDeploy', $applicationDeploy);
+        
+        $applicationRemove = $retFactory::factory('applicationRemove', '1');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ApplicationRemove', $applicationRemove);
+
+        $applicationUpdate = $retFactory::factory('applicationUpdate', '1', 'mypackage.zpk');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ApplicationUpdate', $applicationUpdate);
+        
+        $applicationSynchronize = $retFactory::factory('applicationSynchronize', '1');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ApplicationSynchronize', $applicationSynchronize);
+        
+        $applicationRollback = $retFactory::factory('applicationRollback', '1');
+        $this->assertInstanceOf('\ZendServerAPI\Method\ApplicationRollback', $applicationRollback);
+        
+        $applicationGetStatus = $retFactory::factory('applicationGetStatus', array('1'));
+        $this->assertInstanceOf('\ZendServerAPI\Method\ApplicationGetStatus', $applicationGetStatus);
+    }
+    
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage The method applicationUpdate is not available
+     */
+    public function testWebApiMethodFromNewerApiVersion()
+    {
+        $request = Startup::getRequest("ZS50");
+        $config = $request->getConfig();
+        $webApiVersionFactory = new \ZendServerAPI\Factories\WebApiVersionFactory();
+        $webApiVersionFactory->setConfig($config);
+        
+        $this->assertEquals($config->getApiVersion(), \ZendServerAPI\Version::ZS50);
+        
+        $retFactory = $webApiVersionFactory->getCommandFactory();
+        $applicationUpdate = $retFactory::factory('applicationUpdate', '1', 'mypackage.zpk');
     }
     
     /**

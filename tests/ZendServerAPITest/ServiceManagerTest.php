37201026dc976c4617706a9c78405e2e6fef5158
ingo <ingo.walz@googlemail.com>
2012-12-11 20:10:09 +0100

    Factory tests
    Change-Id: I64b791ef33281833dcca10210a4d6323ebe35732

--- tests/ZendServerAPITest/ServiceManagerTest.php
+++ tests/ZendServerAPITest/ServiceManagerTest.php
@@ -2,6 +2,14 @@
 
 namespace ZendServerAPITest;
 
+use ZendService\ZendServerAPI\Method\ApplicationGetStatus;
+
+use ZendService\ZendServerAPI\Method\ClusterAddServer;
+
+use ZendService\ZendServerAPI\Method\ApplicationDeploy;
+
+use ZendService\ZendServerAPI\Deployment;
+
 use ZendService\ZendServerAPI\ServiceManagerConfig;
 
 use ZendService\ZendServerAPI\Method\ClusterGetServerStatus;
@@ -15,11 +23,11 @@ class ServiceManagerTest extends \PHPUnit_Framework_TestCase
 {
     public function testForAPIVersion10CommandsInSM()
     {
-        $server = new Server("example62");
+        $server = new Server("ZS51");
         $server->setConfigFile(__DIR__.'/../_files/config/config.php');
         $sm = $server->getServiceManager();
-        $conf = $server->getServiceManagerConfig()->registerAPIVersionFactories();
         $this->assertTrue($sm->get('clusterGetServerStatus') instanceof ClusterGetServerStatus);
+        $this->assertTrue($sm->get('clusterAddServer') instanceof ClusterAddServer);
     }
     
     public function testForCorrectLoggerSetup()
@@ -28,14 +36,19 @@ class ServiceManagerTest extends \PHPUnit_Framework_TestCase
         $server->setConfigFile(__DIR__.'/../_files/config/config.php');
         $sm = $server->getServiceManager();
         
-        if($server->canConnect())
-            var_dump($server->getSystemInfo());
-        
         $this->assertTrue($sm->get('logger')->getWriters()->current() instanceof \Zend\Log\Writer\Stream);
         $server->disableLogging();
         $this->assertTrue($sm->get('logger')->getWriters()->current() instanceof \Zend\Log\Writer\Mock);
         $server->enableLogging();
         $this->assertTrue($sm->get('logger')->getWriters()->current() instanceof \Zend\Log\Writer\Stream);
+    }
+    
+    public function testDeploy()
+    {
+        $server = new Server("example62");
+        $server->setConfigFile(__DIR__.'/../_files/config/config.php');
+        $sm = $server->getServiceManager();
+        $this->assertTrue($sm->get('applicationGetStatus') instanceof ApplicationGetStatus);
     }
     
 }

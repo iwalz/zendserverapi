fbe3630a9c8956735bf554e5da39467e080c2404
ingo <ingo.walz@googlemail.com>
2013-01-05 01:48:56 +0100

    Added zdpack assets

---
+++ library/ZendService/ZendServerAPI/Zdpack/Assets/data/index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+	<TITLE>Assets/ - Zend sample package</TITLE>
+</HEAD>
+<BODY style="text-align: center">
+	<H1>Hello world</H1>
+	<DIV style="font-size: 40px">
+		Assets/ says hello
+	</DIV>
+</BODY>
+</HTML>

e7489aff23f6c57899ef00bfabbd2d47f43f3dd0
ingo <ingo.walz@googlemail.com>
2013-01-28 23:49:57 +0100

    Added jobdetails model

---
+++ library/ZendService/ZendServerAPI/DataTypes/JobDetails.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link           http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright      Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license        http://framework.zend.com/license/new-bsd New BSD License
+ * @package        Zend_Service
+ */
+
+namespace ZendService\ZendServerAPI\DataTypes;
+
+/**
+ * JobDetails model implementation.
+ *
+ * @license        http://framework.zend.com/license/new-bsd New BSD License
+ * @link           http://github.com/zendframework/zf2 for the canonical source repository
+ * @author         Ingo Walz <ingo.walz@googlemail.com>
+ * @category       Zend
+ * @package        Zend_Service
+ * @subpackage     ZendServerAPI
+ */
+class JobDetails extends DataType
+{
+    /**
+     * @var int
+     */
+    protected $jobId = null;
+    /**
+     * @var array
+     */
+    protected $vars = array();
+    /**
+     * @var string
+     */
+    protected $output = null;
+    /**
+     * @var array
+     */
+    protected $httpHeaders = array();
+    /**
+     * @var string
+     */
+    protected $error = null;
+
+    /**
+     * @param string $error
+     */
+    public function setError($error)
+    {
+        $this->error = $error;
+    }
+
+    /**
+     * @return string
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param array $httpHeaders
+     */
+    public function setHttpHeaders($httpHeaders)
+    {
+        $this->httpHeaders = $httpHeaders;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHttpHeaders()
+    {
+        return $this->httpHeaders;
+    }
+
+    /**
+     * @param int $jobId
+     */
+    public function setJobId($jobId)
+    {
+        $this->jobId = $jobId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJobId()
+    {
+        return $this->jobId;
+    }
+
+    /**
+     * @param string $output
+     */
+    public function setOutput($output)
+    {
+        $this->output = $output;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    /**
+     * @param array $vars
+     */
+    public function setVars($vars)
+    {
+        $this->vars = $vars;
+    }
+
+    /**
+     * @return array
+     */
+    public function getVars()
+    {
+        return $this->vars;
+    }
+}

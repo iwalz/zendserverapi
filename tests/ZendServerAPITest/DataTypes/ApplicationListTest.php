151a7dedb608c1358f40fb0c4b31f72eb5ee2ac7
ingo <ingo.walz@googlemail.com>
2013-03-04 22:55:23 +0100

    applicationGetByName tests

--- tests/ZendServerAPITest/DataTypes/ApplicationListTest.php
+++ tests/ZendServerAPITest/DataTypes/ApplicationListTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace ZendServerAPITest\DataTypes;
 
+use ZendService\ZendServerAPI\Deployment;
+
 /**
  * test case.
  */
@@ -19,6 +21,30 @@ class ApplicationListTest extends \PHPUnit_Framework_TestCase
         $applicationList->addApplicationInfo($applicationInfo);
         
         $this->assertEquals(array($applicationInfo), $applicationList->getApplicationInfos());
+    }
+
+    public function testGetApplicationByName()
+    {
+        $strApplicationList = file_get_contents(__DIR__ . '/TestAsset/applicationList_multiple');
+        $deployment = new Deployment('example62');
+        $adapter = new \Zend\Http\Client\Adapter\Test();
+        $adapter->setResponse($strApplicationList);
+        $deployment->getRequest()->setClientAdapter($adapter);
+
+        $applicationList = $deployment->applicationGetStatus();
+        $result = $applicationList->getApplicationInfoByName('example2');
+        $this->assertInstanceOf('\ZendService\ZendServerAPI\DataTypes\ApplicationInfo', $result);
+
+        $this->assertEquals('example2', $result->getAppName());
+        $this->assertEquals('120', $result->getId());
+
+        $result = $applicationList->getApplicationInfoByName('foo');
+        $this->assertFalse($result);
+
+
+        $result = $applicationList->getApplicationInfoByName(null);
+        $this->assertFalse($result);
+
     }
 }
 
